67e24120f3bf31723fcd160a47b056a9345f65cd
smalipeddi <sunitha_malipeddi@yahoo.com>
2015-12-01 17:02:34 -0500

    working shopping list app

--- js/script.js
+++ js/script.js
@@ -1,18 +1,48 @@
 $(document).ready(function(){
-   
+    //if input field in clicked, means focussed
     $('#grocery').focus(function(){
+      //clear its text 
        $( "#grocery" ).val('');
     });
-    $('#grocery').keypress(
-        function(){
-          console.log("key presses is " + $('#grocery').val() );
+    //set global variable for counter
+    var idCounter=1;
+
+    //if add button is clicked
+    $("#add").click(function(){
+
+        //get the html text entered in teh input field
+        var val = $("#grocery").val();
+
+        //append that text to the 
+        $("ul").append ("<br/>"+ "<div> <input id='chk_" + idCounter + "' type='checkbox' value='" + val + "' />" + "<label for='chk_" + idCounter + "'>" + val + "</label></div>");
+           idCounter ++;
+
     });
 
-    $('.cart').click(
-        function(){
+    $("#remove").click(function(){
+         //check if a checkbox is checked 
+         if($('[type="checkbox"]').is(":checked")){
+            //find its parent elements
+            var dom_element = $('[type="checkbox"]').parent();
+             //traverse through each element in its parent tree 
+            for (var i = 0; i < dom_element.length ; i++)
+            {
+              //Find which checkbox is checked
+              if(dom_element[i].childNodes[1].checked == true){
+                  //remove only node form tree              
+                  dom_element[i].remove();
+              }
+            }
 
-          $("ol").append("<li>" +$( "#grocery" ).val() + "</li>");
-                     
-          $('.add_remove').append("<div> <i class='fa fa-trash fa-2x cart'></i> </div>");
-        });
+           }else 
+           {
+
+           }
+       
+           return false;
+    });
+
+    
 });
+
+
